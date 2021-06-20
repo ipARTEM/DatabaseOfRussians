a55b4ac54c031cf208b7eb6dbf7ee284cb6dfaea
Artem <a.khimin@yandex.ru>
2021-06-20 23:23:32 +0300

    SQL

--- DBOfRussians.Models/Citizen.cs
+++ DBOfRussians.Models/Citizen.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace DBOfRussians.Models
 {
     public class Citizen
     {
         public int Id { get; set; }
 
+
+        
         public string Name { get; set; }
 
+    
         public string Surnames { get; set; }
+
+        
         public string Patronymic { get; set; }
         public long SNILS { get; set; }
 
---
+++ DBOfRussians.Services/Migrations/20210620200041_InitDB2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DBOfRussians.Services.Migrations
+{
+    public partial class InitDB2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Citizens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Surnames = table.Column<string>(nullable: true),
+                    Patronymic = table.Column<string>(nullable: true),
+                    SNILS = table.Column<long>(nullable: false),
+                    INN = table.Column<long>(nullable: false),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    DateOfDeath = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Citizens", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Citizens");
+        }
+    }
+}
